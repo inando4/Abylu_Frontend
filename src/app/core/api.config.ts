fc1bb22c9387668a76d1e77447e7418dda7737f1
Luis Rodrigo <lperezto@unsa.edu.pe>
2026-04-05 18:48:54 -0500

    Fix: Api backend, Beta v1.10

--- src/app/core/api.config.ts
+++ src/app/core/api.config.ts
@@ -4,4 +4,4 @@
  * Equivalente a tener spring.datasource.url en application.properties.
  * Si mañana tu backend cambia de puerto o dominio, solo cambias aquí. En local es: http://localhost:8090/api
  */
-export const API_BASE_URL = 'http://localhost:8090/api';
+export const API_BASE_URL = 'https://software-api-emyc.onrender.com/api';
